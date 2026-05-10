8a0fa0c61a3160c2b28efca602f59a9ddabb4814
tshiamobatshidi0-arch <tshiamobashidi0@gmail.com>
2026-05-10 20:51:27 +0200

    made the app to be serverless

--- backend/src/controller/user.controller.js
+++ backend/src/controller/user.controller.js
@@ -1,3 +1,23 @@
-export const signUp = (req,res)=>{
-    res.send('<h1>sign_up</h1>')
+
+import { Resend } from 'resend';
+import jwt from 'jsonwebtoken';
+import { createContact } from '../../../chat-app/src/db/db.models.js';
+
+
+const  RESEND_API_KEY = re_Xi2jrcMW_AzV4FP72DTXa8N7JWmBQWe4d //env file content
+const resend = new Resend(RESEND_API_KEY);
+
+export const signUp =async (req,res)=>{
+    const { email , phoneNumber, firstName, lastName, photoUrl } = req.body;
+
+    await createContact(phoneNumber,email, firstName, lastName, photoUrl);
+
+    resend.emails.send({
+            from: 'batshiditshiamo@gmail.com',
+            to: email,
+            subject: 'Hello World',
+            html: '<p>Congrats on sending your <strong>first email</strong>!</p>'
+     });
+    
+
 }
