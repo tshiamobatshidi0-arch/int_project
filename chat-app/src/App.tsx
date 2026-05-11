ebeb8372667f75ac0a21d4da5a61141ed1b4977f
tshiamobatshidi0-arch <tshiamobashidi0@gmail.com>
2026-05-11 11:37:44 +0200

    functionality update

--- chat-app/src/App.tsx
+++ chat-app/src/App.tsx
@@ -1,13 +1,30 @@
 import { useState } from 'react'
 import viteLogo from './assets/vite.svg'
-import { createBrowserRouter, createRoutesFromElements ,Route,Router,RouterProvider } from 'react-router'
+import { createBrowserRouter, createRoutesFromElements ,Route,Outlet,RouterProvider } from 'react-router'
+
+function SideBar(){
+  return (
+    <div className="">
+      <h2>Chat App</h2>
+      <ul>
+        <li>Home</li>
+        <li>Profile</li>
+        <li>Settings</li>
+      </ul>
+     <Outlet/>
+    </div>
+  )
+}
+
 
 
 function App() {
 
   const router = createBrowserRouter(
     createRoutesFromElements(
-      <Route path="/" element={<h1>Welcom to the chat app</h1>}/>
+      <Route path="/" element={<SideBar/>}>
+         <Route index element={<h1>Welcome to the chat app</h1>}/>
+      </Route>
     )
   )
 
